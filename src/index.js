d97fd4d920ac87fd1b9d948f6788a009a0e9205e
merzat68 <85101629+merzat68@users.noreply.github.com>
2022-08-18 15:55:07 +0430

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./css/style.scss";
-
-// Our modules / classes
-import * as bootstrap from "bootstrap";
-import Search from "./modules/search";
-import InstagramFetch from "./modules/instagram-fetch";
-
-const magicalSearch = new Search();
-const IGFetch = new InstagramFetch();
